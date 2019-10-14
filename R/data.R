cc89fa45446697e56eb94e451e67e988a203ec06
Adhaar Sharma <asharma327@gmail.com>
2019-10-14 18:59:13 -0400

    v 2.0

--- R/data.R
+++ R/data.R
@@ -1,19 +1,120 @@
 #'
-#'@param output_format
+#' @param output_format
 #' @export
 
 # devtools::document()
-check_paramter_errors <- function(output_format, accessKey, secretKey){
+
+#'
+#' @export
+
+get_base_url <- function(){
+  # return("http://127.0.0.1:8000/data/")
+  return("https://api.apispreadsheets.com/data/")
+}
+
+#'
+#' @export
+
+get_output_format <- function(output_format){
+  if (is.null(output_format)){
+    return("jsonRow")
+  }
+  else{
+    return(output_format)
+  }
+}
+
+#'
+#' @export
+
+create_get_url <- function(url, output_format, access_key, secret_key, rows){
+  if (!is.null(access_key) & !is.null(secret_key)){
+    new_url = paste(url, "?accessKey=", access_key, "&secretKey=", secret_key, sep="")
+    data_prefix = "&"
+  }
+  else{
+    new_url = url
+    data_prefix = "?"
+  }
+    
+  data_query_param = paste(data_prefix, "dataFormat=", output_format, sep="")
+  output_url = paste(new_url, data_query_param, sep="")
+  
+  final_url = output_url
+  
+  if (!is.null(rows)){
+    if (grepl('?', url)){
+      final_url = paste(output_url, "&rows=", as.character(rows), sep="")
+    }
+    else{
+      final_url = paste(output_url, "?rows=", as.character(rows), sep="")
+    }
+  }
+    
+  return(final_url)
+}
+
+#'
+#' @export
+
+create_post_headers <- function(access_key, secret_key){
+  headers <- c()
+  
+  if (!is.null(access_key) & !is.null(secret_key)){
+    headers <- c('accessKey' = access_key, 'secretKey' = secret_key)
+  }
+  
+  return(headers)
+}
+
+#'
+#' @export
+
+check_paramter_errors <- function(output_format, accessKey, secretKey, rows=NULL, data=NULL){
   if(!is.null(output_format)){
     if(!(output_format %in% c('jsonRow', 'jsonColumn', 'matrix'))){
       stop("Output format must be jsonRow, jsonColumn or matrix or not used. Default is jsonRow")
     }
   }
-
+  
   if ((is.null(accessKey) & !is.null(secretKey)) | (is.null(secretKey) & !is.null(accessKey))){
     stop("Both access and secret key parameters must have values or not be used")
   }
+  
+  if (!is.null(rows)){
+    if(is.na(as.numeric(rows))){
+      stop("Rows parameter must be an integer")
+    }
+  }
+  
+  if (!is.null(data)){
+    if (nrow(data) == 0){
+      stop("Data must not be empty")
+    }
+  }
 }
+
+#'
+#' @export
+
+post_data <- function(file_id, data_format=NULL, access_key=NULL, secret_key=NULL, data=NULL){
+  base_url <- get_base_url()
+  
+  check_paramter_errors(data_format, access_key, secret_key, data=data)
+  
+  output <- get_output_format(data_format)
+  
+  url <- paste(base_url, as.character(file_id), "/", sep="")
+  headers <- create_post_headers(access_key, secret_key)
+
+  r <- httr::POST(url=url, httr::add_headers(headers), body=list(format = output, data=data), encode="json")
+  
+  post_status_code <- httr::status_code(r)
+  
+  return(post_status_code)
+}
+
+
 
 #'
 #' @export
@@ -30,58 +131,31 @@ return_data <- function(output_format, jsonResponse){
 #'
 #' @export
 
-data <- function(file_id, output_format=NULL, accessKey=NULL, secretKey=NULL){
-  base_url <- "https://api-woyera.com/api/data/"
+get_data <- function(file_id, data_format=NULL, access_key=NULL, secret_key=NULL, rows=NULL){
+  base_url <- get_base_url()
 
-  check_paramter_errors(output_format, accessKey, secretKey)
-
-  if (is.null(output_format)){
-    output = "jsonRow"
+  check_paramter_errors(data_format, access_key, secret_key, rows=rows)
+  
+  output <- get_output_format(data_format)
+  
+  id_url <- paste(base_url, as.character(file_id), "/", sep="")
+  url <- create_get_url(id_url, output, access_key, secret_key, rows)
+  
+  r <- httr::GET(url=url)
+  get_status_code <- httr::status_code(r)
+  
+  if (get_status_code == 200){
+    jsonResponse <- jsonlite::fromJSON(httr::content(r, "text"))
+    return (return_data(output, jsonResponse))
+  }
+  else if (get_status_code == 400){
+    stop("The file is private. Please provide access and secret keys")
+  }
+  else if (get_status_code == 404){
+    stop("This file ID does not exist. Please find the correct ID from your dashboard")
   }
   else{
-    output = output_format
-  }
-
-  url <- paste(base_url, as.character(file_id), "/", output, "/", sep="")
-
-  if (is.null(accessKey) & is.null(secretKey)){
-    r <- httr::GET(url=url)
-    get_status_code <- httr::status_code(r)
-
-    if (get_status_code == 200){
-      jsonResponse <- jsonlite::fromJSON(httr::content(r, "text"))
-      return (return_data(output, jsonResponse))
-    }
-    else if (get_status_code == 400){
-      stop("The file is private. Please provide access and secret keys")
-    }
-    else if (get_status_code == 404){
-      stop("This file ID does not exist. Please find the correct ID from your dashboard")
-    }
-    else{
-      stop("There was something wrong on our server. Try again or contact us at info@apispreadsheets.com if the problem persists")
-    }
-
-  }
-  else{
-    headers <- c('accessKey' = accessKey, 'secretKey' = secretKey)
-    r <- httr::POST(url=url, httr::add_headers(headers), encode="json")
-
-    post_status_code = httr::status_code(r)
-
-    if (post_status_code == 200){
-      jsonResponse <- jsonlite::fromJSON(httr::content(r, "text"))
-      return (return_data(output,jsonResponse))
-    }
-    else if (post_status_code == 401){
-      stop("The Access or Secret key is invalid")
-    }
-    else if (post_status_code == 404){
-      stop("This file ID does not exist or is not your file. Please find the correct ID from your dashboard")
-    }
-    else{
-      stop("There was something wrong on our server. Try again or contact us at info@apispreadsheets.com if the problem persists")
-    }
+    stop("There was something wrong on our server. Try again or contact us at support@apispreadsheets.com if the problem persists")
   }
 }
 
